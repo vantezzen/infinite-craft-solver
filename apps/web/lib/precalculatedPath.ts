c5af6ca7bb652187d2fce53ec5b74e334e0e2ceb
Bennett <hello@vantezzen.io>
2024-02-20 10:57:39 +0100

    Update precalculatedPath.ts

--- apps/web/lib/precalculatedPath.ts
+++ apps/web/lib/precalculatedPath.ts
@@ -1,6 +1,7 @@
 import { kv } from "@vercel/kv";
+import { Recipe } from "./Finder";
 
 export async function getPrecalculatedPath(item: string) {
   const cachePath = `recipe-${item}`;
-  return await kv.get<string>(cachePath);
+  return await kv.get<Recipe[]>(cachePath);
 }
