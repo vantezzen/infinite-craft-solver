720f0767f8f4b1c10251c3bc782859a61f4a3327
Bennett <hello@vantezzen.io>
2024-02-22 15:29:12 +0100

    Add caching for item list

--- apps/web/lib/items.ts
+++ apps/web/lib/items.ts
@@ -1,11 +1,23 @@
 import { sql } from "@vercel/postgres";
+import { kv } from "@vercel/kv";
 
 export async function getItemList() {
+  const cachePath = "item-list";
+  const cached = await kv.get<string[]>(cachePath);
+  if (cached) {
+    return cached;
+  }
+
   const items = await sql<{ result: string }>`
     SELECT DISTINCT result FROM "Recipe"
   `;
 
-  return items.rows
+  const itemNames = items.rows
     .map((item) => item.result)
     .filter((item) => item !== "Nothing");
+
+  await kv.set(cachePath, itemNames, {
+    ex: 60 * 60 * 24,
+  });
+  return itemNames;
 }
