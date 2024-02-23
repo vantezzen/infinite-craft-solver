d84ece7cb7ae998967674b9254858b8b1f2d717d
Bennett <hello@vantezzen.io>
2024-02-23 17:24:21 +0100

    Convert update to streamed response

--- apps/web/app/api/update/route.ts
+++ apps/web/app/api/update/route.ts
@@ -13,45 +13,62 @@ export async function GET(request: Request) {
     });
   }
 
-  const recipes: Recipe[] = [];
-  let offset = 0;
-  const limit = 10000;
-  let hasMore = true;
+  const encoder = new TextEncoder();
+  const streamResponse = new ReadableStream({
+    async start(controller) {
+      const keepAlive = setInterval(() => {
+        controller.enqueue(encoder.encode("."));
+      }, 1000);
+      const timeout = setTimeout(() => {
+        clearInterval(keepAlive);
+        controller.enqueue(encoder.encode("TIMEOUT"));
+        controller.close();
+      }, 60000);
 
-  while (hasMore) {
-    const recipeResult = await sql<Recipe>`
+      const recipes: Recipe[] = [];
+      let offset = 0;
+      const limit = 10000;
+      let hasMore = true;
+
+      while (hasMore) {
+        const recipeResult = await sql<Recipe>`
       SELECT * FROM "Recipe"
       ORDER BY result
       LIMIT ${limit} OFFSET ${offset}
     `;
-    if (recipeResult.rows.length > 0) {
-      recipes.push(
-        ...recipeResult.rows.map((row) => ({
-          first: row.first,
-          second: row.second,
-          result: row.result,
-        }))
-      );
-      offset += recipeResult.rows.length;
-    } else {
-      hasMore = false;
-    }
-  }
+        if (recipeResult.rows.length > 0) {
+          recipes.push(
+            ...recipeResult.rows.map((row) => ({
+              first: row.first,
+              second: row.second,
+              result: row.result,
+            }))
+          );
+          offset += recipeResult.rows.length;
+        } else {
+          hasMore = false;
+        }
+      }
 
-  const client = new S3Client({
-    endpoint: process.env.S3_ENDPOINT,
-    region: process.env.S3_REGION,
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY!,
-      secretAccessKey: process.env.S3_SECRET_KEY!,
+      const client = new S3Client({
+        endpoint: process.env.S3_ENDPOINT,
+        region: process.env.S3_REGION,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY!,
+          secretAccessKey: process.env.S3_SECRET_KEY!,
+        },
+      });
+      const command = new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET!,
+        Key: "recipes.json",
+        Body: JSON.stringify(compressRecipes(recipes)),
+      });
+      await client.send(command);
+
+      clearInterval(keepAlive);
+      clearTimeout(timeout);
+      controller.close();
     },
   });
-  const command = new PutObjectCommand({
-    Bucket: process.env.S3_BUCKET!,
-    Key: "recipes.json",
-    Body: JSON.stringify(compressRecipes(recipes)),
-  });
-  await client.send(command);
-
-  return new Response("ok");
+  return new Response(streamResponse);
 }
